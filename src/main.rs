ed63b237fd486c24c09a21c1f4d906adcf0f1322
Jan Vandepitte <jan.vandepitte@centric.eu>
2017-08-23 16:48:37 +0200

    clapapp

---
+++ src/main.rs
@@ -0,0 +1,60 @@
+extern crate web3;
+extern crate clap;
+
+use clap::{Arg, App, SubCommand};
+
+fn main() {
+    let matches = App::new("mango-admin")
+                    .version("1.0")
+                    .author("Yen")
+                    .arg(Arg::with_name("repo")
+                        .short("R")
+                        .long("repo")
+                        .value_name("REPO")
+                        .help("Repository address")
+                        .takes_value(true))
+                    /*.arg(Arg::with_name("INPUT")
+                               .help("Sets the input file to use")
+                               .required(true)
+                               .index(1))
+                          .arg(Arg::with_name("v")
+                               .short("v")
+                               .multiple(true)
+                               .help("Sets the level of verbosity"))
+                          .subcommand(SubCommand::with_name("test")
+                                      .about("controls testing features")
+                                      .version("1.3")
+                                      .author("Someone E. <someone_else@other.com>")
+                                      .arg(Arg::with_name("debug")
+                                          .short("d")
+                                          .help("print debug information verbosely")))*/
+                          .get_matches();
+
+
+   /*  .usage('Usage: $0 [command]')
+  .option('R', {
+    alias: 'repo',
+    describe: 'Repository address',
+    type: 'string'
+  })
+  .option('admin', {
+    describe: 'Treat as administrator',
+    type: 'boolean'
+  })
+  .option('account', {
+    describe: 'Sender account (a current administrator)',
+    type: 'string'
+  })
+  .global([ 'repo', 'admin' ])
+  .command('status', 'Check status of repository')
+  .command('create', 'Create repository')
+  .command('obsolete', 'Mark repository obsolete')
+  .command('authorize <address>', 'Authorize account with write access')
+  .command('deauthorize <address>', 'Deauthorize account')
+  .strict()
+  .version()
+  .showHelpOnFail(false, 'Specify --help for available options')
+  .help()
+  .demand(1, 'Must provide a command')*/
+
+}
